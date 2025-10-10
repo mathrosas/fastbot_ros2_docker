8296c206b555170e040a4a34b46c242d5adfa597
mathrosas <mathiasrosas@gmail.com>
2025-10-10 12:59:45 +0000

    Fix typos

--- real/entrypoint.sh
+++ real/entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI=/var/lib/theconstruct.rrl/cyclonedds.xml
 source "/opt/ros/$ROS_DISTRO/setup.bash"
-source "/colcon_ws/install/setup.bash"
+source "/ros2_ws/install/setup.bash"
 exec bash -c "$@"
